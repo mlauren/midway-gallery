f0d1f2e9069b056b19a30d16bb1f158631e4dcaa
Lauren Maxwell <mlmxwell@gmail.com>
2014-11-19 15:33:35 -0600

    Adds new font.

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -10,9 +10,10 @@
 	    {{ HTML::style('/packages/css/styles.css') }}
 	    {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css') }}
 
+	    {{ HTML::link('http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400') }}
+
 	</head>
 	<body>
-
 		<div class="container">
 			<header class="row">
 				<div class="col-md-10">
