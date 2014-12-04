6bf4efecf597d71ab71146a883ba774ce387c914
Lauren Maxwell <mlmxwell@gmail.com>
2014-12-03 18:33:19 -0600

    Adds changes to fillable property and fixes the GOD DAMN SPACING.

--- app/models/Slides.php
+++ app/models/Slides.php
@@ -1,6 +1,10 @@
 <?php
   class Slides extends Eloquent
   {
-  	protected $table = 'slides';
+    protected $table = 'slides';
+
+    protected $fillable = array(
+      'slide_image', 'slide_title', 'slide_text', 'slide_order'
+    );
 
   }
