62b4e8b569405b8c6448961d8f2b12f3cacfd0d5
Lauren Maxwell <mlmxwell@gmail.com>
2014-12-05 18:42:07 -0600

    Adds templating changes and CSS styles.

--- app/views/slideshow/add.blade.php
+++ app/views/slideshow/add.blade.php
@@ -1,44 +1,69 @@
 <div class="slide-container unassigned col-md-11"{{ isset($slide) ? ' data-order="' . $slide->slide_order . '" data-id="' . $slide->id . '"'  : '' }}>
-
-  <div class="col-md-1">
-    <div class="drag-order"><i class="fa fa-arrows fa-3x"></i></div>
+  <div class="col-md-1 col-sm-1 col-xs-1 icon-column-padding">
+    <div class="drag-order"><i class="fa fa-arrows fa-2x"></i></div>
   </div>
 
-  <div class="col-md-11">
+  <div class="col-md-11 col-sm-11 col-xs-11 border-left">
 
-    <div class="col-md-4">
-      <h4><a href="/" class="upload-image"> <i class="fa fa-plus-circle fa-lg"></i> Upload Image</a></h4>
+    <div class="col-md-6 col-sm-6 col-xs-6">
 
-      <div id="slide-image">
-        {{-- Hide this and allow it to be activated by hidden field --}}
-        <div class="form-group">
-          <label for="file">File input</label>
-          <input type="file" id="file">
-          <p class="help-block">Upload some type of image.</p>
+      <div id="img-preview-container" class="col-md-5 col-xs-5 col-sm-5">
+        @if ( isset($slide) )
+          @if(null != $slide->media)
+            <img id="img-preview" class="img-responsive" src="{{ $slide->media->img_min }}" />
+            <h4 class="img-preview-remove"><a href="" >X</a></h4>
+            <h4 class="img-preview-overlay">Preview</h4>
+          @else
+            <div id="img-preview" class="panel panel-default">
+              <div class="panel-body"> No Image Available </div>
+            </div>
+          @endif
+        @else
+          <div id="img-preview" class="panel panel-default">
+            <div class="panel-body"> No Image Available </div>
+          </div>
+        @endif
+      </div>
+      <div class="col-md-7 col-sm-7 col-xs-7 border-left">
+        <div id="slide-image">
+          {{-- Hide this and allow it to be activated by hidden field --}}
+          <div class="form-group">
+            <label for="file">Slide Image</label>
+            <input type="file" id="file">
+            <p class="help-block">Upload some type of image.</p>
+          </div>
         </div>
       </div>
-      @if(Media::where('slide_id', $slide->id)->firstOrFail())
-        <img id="img-preview" src="{{ Media::where('slide_id', $slide->id)->firstOrFail()->img_min }}" />
-      @endif
+
     </div>
 
-    <div class="col-md-8">
-      <div class="col-md-5">
+    <div class="col-md-5 col-sm-5 col-xs-5 border-left border-right">
+
+      <div class="col-md-12 form-group info-add">
         {{ Form::label('title', 'Title (Optional)', array('class' => 'control-label')); }}
-        {{ Form::text('title', null, array('class'=>'form-control')) }}
+        @if (isset($slide) && isset($slide->slide_title))
+          {{ Form::text('title', $slide->slide_title, array('class'=>'form-control')) }}
+        @else
+          {{ Form::text('title', null, array('class'=>'form-control')) }}
+        @endif
       </div>
-      <div class="col-md-5">
+      <div class="col-md-12 form-group info-add">
         {{ Form::label('details', 'Subtitle (Optional)', array('class' => 'control-label')); }}
-        {{ Form::text('details', null, array('class'=>'form-control')) }}
+        @if (isset($slide) && isset($slide->slide_text))
+          {{ Form::text('details', $slide->slide_text, array('class'=>'form-control')) }}
+        @else
+          {{ Form::text('details', null, array('class'=>'form-control')) }}
+        @endif
       </div>
-      <div class="col-md-2">
-        <a href="" class="remove-slide"> <i class="fa fa-times fa-lg"></i></a>
-      </div>
+
+    </div>
+    <div class="col-md-1 col-sm-1 col-xs-1 icon-column-padding">
       @if(isset($slide))
         {{ Form::hidden('data-id', $slide->id) }}
       @else
         {{ Form::hidden('data-id', null) }}
       @endif
+      <a href="" class="remove-slide"> <i class="fa fa-times fa-2x"></i></a>
     </div>
   </div>
   <hr class="col-md-12" />
--- app/views/slideshow/configure.blade.php
+++ app/views/slideshow/configure.blade.php
@@ -5,6 +5,8 @@
 @section('content')
   <div class="col-md-10">
     <h2>Slides</h2>
+    <p class="help-block">Images need to be this size. Slides will not show up on the homepage unless they have an image attached to them.</p>
+    <p class="help-block">Do not add text to slides unless you want it to show on the slide.</p>
     <h3><a href="/" class="add-slide-object"><i class="fa fa-plus-circle fa-lg"></i> Add New</a></h3>
   </div>
   {{ Form::open() }}
@@ -18,9 +20,6 @@
       @endif
     </div>
 
-    <div class="col-md-12">
-      {{ Form::submit('Submit', array('class'=>'btn btn-large btn-default')) }}
-    </div>
   {{ Form::close() }}
 @stop
 
