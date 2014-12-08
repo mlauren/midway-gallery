20ae5fc6b6e6dd421c86413a02a1c70a6bd02e5d
Lauren Maxwell <mlmxwell@gmail.com>
2014-12-08 14:38:08 -0600

    Adds new media exhibit functionality for in page AJAX.

--- public/packages/custom_javascripts/media-add-new-exhibit.js
+++ public/packages/custom_javascripts/media-add-new-exhibit.js
@@ -1,94 +1,140 @@
-(function( mediable, $, undefined ) { 
+// Adds ajax functions in a Class
+(function( ajaxable, $, undefined ) {
 
-  // Get our form file input element and set formdata to false in order 
-  // to display errors for crappy browsers.
-  var input = document.getElementById("file[]"),
-    formdata = false,
-    ex_id;
-  if ( window.FormData ) {
-    formdata = new FormData();
+  ajaxable.addImage = function(formdata) {
+    return $.ajax({
+            url: "/exhibit-media-add",
+            type: "POST",
+            data: formdata,
+            cache: false,
+            processData: false,
+            dataType: 'json',
+            contentType: false,
+            async: true
+          });
   }
 
+  ajaxable.removeImage = function(formdata) {
+    return $.ajax({
+              url: "/slide-remove-media",
+              type: "POST",
+              data: formdata,
+              cache: false,
+              processData: false,
+              dataType: 'json',
+              contentType: false,
+              async: true
+            });
+  }
+
+  ajaxable.addDraft = function() {
+    return $.ajax({
+              url: "/exhibit-add-empty",
+              type: "POST",
+              cache: false,
+              processData: false,
+              dataType: 'json',
+              contentType: false,
+              async: true
+            });
+  }
+
+  ajaxable.reorderImages = function(formdata) {
+    // Ajax function to update data order for slides
+    return $.ajax({
+              url: "/slide-reorder",
+              type: "POST",
+              data: formdata,
+              cache: false,
+              processData: false,
+              dataType: 'json',
+              contentType: false,
+              async: true
+            });
+  }
+
+}( window.ajaxable = window.ajaxable || {}, jQuery ));
+
+
+
+(function( mediable, $, undefined ) {
+
   mediable.add = function() {
-    if (input.addEventListener) {
-      // Add event listener
-      input.addEventListener("change", function(evt) {
-        var len = this.files.length,
+    // --- add jQuery listener to input --- //
+    return $(document.body).on('change', 'input#file', function(e) {
+
+      var len = this.files.length,
+          formdata = false,
+          el = this,
+          ex_id,
           input = $('input:hidden[name="id"]'),
           img,
           reader,
           file;
-
-        if ( input.val() == 0 || input.val() == "" ) {
-          addExhibitDraft();
-        }
-        addLoadIcon("image-preview-exists");
-
-        formdata.append("id", input.val());
-
-        for ( var i = 0; i < len; i++ ) {
-          file = this.files[i];
-          if (formdata) {
-            // Make sure to add the count into the file[i] array
-            formdata.append("file", file);
-
-            doActionInsideLoop(i, formdata);
-          }
-        }
-      }, false);
-    }
-  }
-
-  function doActionInsideLoop(i) {
-    var input = $('input:hidden[name="id"]');
-    $.ajax({
-      url: "/media-add",
-      type: "POST",
-      data: formdata,
-      cache: false,
-      processData: false,
-      dataType: 'json',
-      contentType: false,
-      async: true
-    }).done(function(response) {
-      removeLoadIcon("image-preview-exists");
-      if ( response.success == true ) {
-        suckInUploadedItem('/' + response.img_min_dest, "New Image " + i, response.media_id);
+      // --- check for formdata support -- // 
+      if ( window.FormData ) {
+        formdata = new FormData();
       }
-    }).always(function(response) {
+      if ( input.val() == 0 || input.val() == "" ) {
 
-      updateMediaOrder('#image-preview-exists', '.img-min-preview', input.val());
-      $('form :file').val('');
-    }).fail(function() {
-      // Put in Error Message if something goes
-      // wrong with removing image
-      var errorTxt = 'Oh snap! Something went wrong with adding one of your images.',
-          htmlString = '<div class="alert alert-danger" role="alert">' + errorTxt + '</div>';
-      $('.feedback-container').html(htmlString);
+        // --- Ajaxable AutoDraft Form function --- //
+        ajaxable.addDraft().done(function(response) {
+          formdata.append("id", response.id);
+          input.val(response.id);
+          ex_id = response.id;
+          $('image-preview-exists').attr('data-ex-id', ex_id);
+          // --- add a loading icon --- //
+          addLoadIcon("image-preview-exists");
+
+          formdata.append("data-id", ex_id);
+          formdata.append("data-type", 'Exhibit');
+
+          for ( var i = 0; i < len; i++ ) {
+            file = el.files[i];
+            if (formdata) {
+              // Make sure to aådd the count into the file[i] array
+              formdata.append("file", file);
+
+              // --- Ajax Form function --- //
+              ajaxable.addImage(formdata)
+                .done(function(response) {
+                  removeLoadIcon("image-preview-exists");
+                  if ( response.success == true ) {
+                    suckInUploadedItem('/' + response.img_min_dest, "New Image " + i, response.media_id);
+                  }
+                }).always(function(response) {
+                  updateMediaOrder('#image-preview-exists', '.img-min-preview', input.val());
+                  // Remove the value of the form
+                  $('form :file').val('');
+                }).fail(function(response) {
+                  // Put in Error Message if something goes
+                  // wrong with adding the image
+                  var errorTxt = 'Oh snap! Something went wrong with adding one of your images.',
+                      htmlString = '<div class="alert alert-danger" role="alert">' + errorTxt + '</div>';
+                  $('.feedback-container').html(htmlString);
+                  removeLoadIcon("image-preview-exists");
+
+
+                  /// ---- Remove the html feedback container ---- ///
+                });
+            }
+          }
+        }).fail(function() {
+          // Put in Error Message if something goes
+          // wrong with adding the image
+          var errorTxt = 'Oh snap! Something went wrong with adding an exhibit.',
+              htmlString = '<div class="alert alert-danger" role="alert">' + errorTxt + '</div>';
+          $('.feedback-container').html(htmlString);
+          removeLoadIcon("image-preview-exists");
+
+
+          /// ---- Remove the html feedback container ---- ///
+        });
+      }
     });
   }
 
-  function addExhibitDraft() {
-    var input = $('input:hidden[name="id"]');
-    var ajax = $.ajax({
-      url: "/exhibit-add-empty",
-      type: "POST",
-      data: formdata,
-      cache: false,
-      processData: false,
-      dataType: 'json',
-      contentType: false,
-      async: false
-    }).done(function(response){
-      formdata.append("id", response.id);
-      input.val(response.id);
-      var ex_id = response.id;
-      $('image-preview-exists').data('exId', response.id);
-    });
-    return ajax;
-  }
-
-// Remove a single record
+  // Remove a single record
   mediable.remove = function() {
     return $('body').on('click', '.media-remove', function (e){
       e.preventDefault();
@@ -204,6 +250,11 @@ mediable.sortable();
  * Load dependent libraries.
  */
 (function ($) {
+
+  $(document.body).on('change', 'input#file', function(e) {
+    mediable.add();
+  });
+
   $('.details-wysi').wysihtml5({
     "stylesheets": []
   });
