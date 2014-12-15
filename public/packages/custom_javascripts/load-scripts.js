22376ea69bb2c3b3bca338a0513443adfede6f02
Lauren Maxwell <mlmxwell@gmail.com>
2014-12-15 17:21:51 -0600

    Cleans up some javascript for generic media-add functionality.

--- public/packages/custom_javascripts/load-scripts.js
+++ public/packages/custom_javascripts/load-scripts.js
@@ -1,59 +1,36 @@
 // Javascript to preview images on pages other than exhibits
 (function( mediable, $, undefined ) {
-  // Get our form file input element and set formdata to false in order
-  // to display errors for crappy browsers.
-  var input = document.getElementById("image"),
-    formdata = false,
-    ex_id;
-  if ( window.FormData ) {
-    formdata = new FormData();
-  }
+
+  // Class to add an image
   mediable.add = function() {
     $('.file').each(function(index, value) {
 
       $(this).bind("change", function(event) {
-        console.log(this.files);
         var len = this.files.length,
+          files = this.files,
           el = this,
           img,
           reader,
           file;
 
+        // Remove any of the remaining images
         $(el).siblings('.image-preview-exists').children('.thumbnail').remove();
         for ( var i = 0; i < len; i++ ) {
           file = this.files[i];
-          if (formdata) {
-
-            // Make sure to add the count into the file[i] array
-            formdata.append("file", this.files[i]);
-          }
-
-          // @todo take this out of the loop.
-          if ( formdata ) {
-            $.ajax({
-              url: "/media-add",
-              type: "POST",
-              data: formdata,
-              cache: false,
-              processData: false,
-              dataType: 'json',
-              contentType: false,
-              async: true
-            }).done(function(response) {
-              removeLoadIcon("image-preview-exists");
-              if ( response.success == true ) {
-                suckInUploadedItem('/' + response.img_min_dest, "New Image " + i, response.media_id);
-                $('form :file').val('');
+          // Match the image and read the file in order to show an image preview.
+          if (!!file.type.match(/image.*/)) {
+            if ( window.FileReader ) {
+              reader = new FileReader();
+              reader.onloadend = function (e) {
+                showUploadedItem(e.target.result, el);
               }
-            }).always(function() {
-              updateMediaOrder('#image-preview-exists', '.img-min-preview');
-            });
+              reader.readAsDataURL(file);
+            }
           }
         }
       });
     });
   }
-
   // Helper function to show the images once the browser has them
   function showUploadedItem(source, el) {
     var htmlString = '<div class="thumbnail"><img src="' +  source + '"/></div>';
@@ -67,7 +44,6 @@
       e.preventDefault();
       var el = $(this),
         url = el.attr('href');
-
       $.ajax({
         type: "POST",
         cache: false,
@@ -76,6 +52,7 @@
         async: false
       }).fail(function(jqXHR, ajaxOptions, thrownError) {
         /* alert(ajaxOptions); */
+        el.closest('.image-preview-exists').removeClass('col-md-3');
       }).done(function(data) {
         el.closest('.image-preview-exists').removeClass('col-md-3');
         el.parent('.thumbnail').remove();
@@ -96,7 +73,9 @@ mediable.add();
   $('.details-wysi').wysihtml5({
     "stylesheets": []
   });
-  $('.datetimepicker6').datetimepicker();
+  if ( typeof datetimepicker == 'function' ) {
+    $('.datetimepicker6').datetimepicker();
+  }
 
 }(jQuery));
 
